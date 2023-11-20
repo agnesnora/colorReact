4df9dd917695882a2bf34c0e6e3c8d6b08bee0e4
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-11-20 15:35:55 +0100

    styling grid

--- src/Header.jsx
+++ src/Header.jsx
@@ -7,13 +7,21 @@ export default function Header() {
   function toggleTheme() {
     setTheme((prevTheme) => (prevTheme == "dark" ? "light" : "dark"));
   }
+  const brickText = ["create", "your", "rainbow", "and", "sunshine"];
+
+  const colorBrickText = brickText.map((item) => (
+    <div key={item} className={`bricktext--cell ${item}`}>
+      {item}
+    </div>
+  ));
 
   return (
     <header>
       <h1>Color Scheme Generator</h1>
+      <div className="bricktext--container">{colorBrickText}</div>
       <h3>Create beautiful color palettes</h3>
       <button className={`${theme}--day--night`} onClick={toggleTheme}>
-        {theme == "light" ? <MdDarkMode /> : <MdOutlineLightMode />}
+        {theme === "light" ? <MdDarkMode /> : <MdOutlineLightMode />}
       </button>
     </header>
   );
