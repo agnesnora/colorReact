48626a13600bd540dd79ac01e09459bf0607fdfb
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-11-24 13:00:24 +0100

    styling

--- src/SavedToProfile.jsx
+++ src/SavedToProfile.jsx
@@ -8,6 +8,20 @@ export default function SavedToProfile({ setProfileOn, savedSchemes, theme }) {
   };
   console.log(savedSchemes);
 
+  // const SchemesEl = () => {
+  //   return savedSchemes.map((scheme, schemeIndex) => (
+  //     <div key={`scheme-${schemeIndex}`} className="scheme--saved">
+  //       {scheme.map((color, colorIndex) => (
+  //         // Inner map for iterating over colors within each scheme
+  //         <div
+  //           key={`color-${colorIndex}`}
+  //           className={`color--${colorIndex}`}
+  //           style={{ backgroundColor: color.value, padding: "3em" }}
+  //         ></div>
+  //       ))}
+  //     </div>
+  //   ));
+  // };
   const SchemesEl = () => {
     return savedSchemes.map((scheme, schemeIndex) => (
       <div key={`scheme-${schemeIndex}`} className="scheme--saved">
@@ -16,8 +30,10 @@ export default function SavedToProfile({ setProfileOn, savedSchemes, theme }) {
           <div
             key={`color-${colorIndex}`}
             className={`color--${colorIndex}`}
-            style={{ backgroundColor: color.value, padding: "3em" }}
-          ></div>
+            style={{ backgroundColor: color.value, padding: "2%" }}
+          >
+            <div className="color--value">{color.value}</div>
+          </div>
         ))}
       </div>
     ));
