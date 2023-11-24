395ce5b81500008c21dbc795af25bd6f600b8f36
Ágnes Kuti-Palasthy <agnesnora@gmail.com>
2023-11-24 14:16:30 +0100

    styling

--- src/color.jsx
+++ src/color.jsx
@@ -1,5 +1,70 @@
-import { useContext } from "react";
+// import { useContext } from "react";
+// import { ColorContext } from "./Generator";
+// export default function Color() {
+//   const { data, theme, formData, loading } = useContext(ColorContext);
+
+//   const colorEl = data
+//     ? data.map((color, index) => (
+//         <div className={`${theme} color--block`} key={color.hex.clean}>
+//           <div
+//             className={`color--cell color--cell--${index}`}
+//             style={{ backgroundColor: color.hex.value }}
+//           ></div>
+//           <input
+//             type="text"
+//             value={color.hex.value}
+//             name="hexValue"
+//             onChange={() => console.log("copied")}
+//             className="hexValue"
+//           />
+//         </div>
+//       ))
+//     : null;
+
+//   return (
+//     <div className="colorscheme--container">
+//       {" "}
+//       <h1>{loading ? formData.mode : formData.newMode}</h1>
+//       <div className="color--palette">{colorEl}</div>
+//     </div>
+//   );
+// }
+import { useContext, useState } from "react";
 import { ColorContext } from "./Generator";
+
+const CopyToClipboard = ({ text }) => {
+  const { theme } = useContext(ColorContext);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyClick = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setIsCopied(true);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+
+    // Reset the "Copied" status after a short delay
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 1500);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        name="hexValue"
+        onChange={() => console.log("copied")}
+        className={`${theme} hexValue`}
+        onClick={handleCopyClick}
+      />
+      {isCopied ? " Copied!" : ""}
+    </div>
+  );
+};
+
 export default function Color() {
   const { data, theme, formData, loading } = useContext(ColorContext);
 
@@ -10,20 +75,13 @@ export default function Color() {
             className={`color--cell color--cell--${index}`}
             style={{ backgroundColor: color.hex.value }}
           ></div>
-          <input
-            type="text"
-            value={color.hex.value}
-            name="hexValue"
-            onChange={() => console.log("copied")}
-            className="hexValue"
-          />
+          <CopyToClipboard text={color.hex.value} />
         </div>
       ))
     : null;
 
   return (
     <div className="colorscheme--container">
-      {" "}
       <h1>{loading ? formData.mode : formData.newMode}</h1>
       <div className="color--palette">{colorEl}</div>
     </div>
